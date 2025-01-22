b10a1e210f85774c03562d63473f273154683f4e
unknown <khalediman96@gmail.com>
2025-01-23 00:19:09 +0400

    adding maxpoolsize instead of poolsize as it is depricted in the newer version of nodejs

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -21,16 +21,28 @@ import mongoose from 'mongoose';
 
 
 
+
+if (!process.env.MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable in .env.local'
+  );
+}
+
 const connectDB = async () => {
   try {
     if (mongoose.connection.readyState === 1) {
       console.log('Using existing MongoDB connection');
       return;
     }
+
+    if (process.env.NODE_ENV === 'development') {
+      mongoose.set('debug', true); // Enable query logging in development
+    }
+
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      poolSize: 10,
+      maxPoolSize: 10, // Set the maximum pool size
     });
     console.log('Connected to MongoDB');
   } catch (error) {
@@ -39,4 +51,13 @@ const connectDB = async () => {
   }
 };
 
-export default connectDB;
+const disconnectDB = async () => {
+  if (mongoose.connection.readyState === 1) {
+    await mongoose.connection.close();
+    console.log('Disconnected from MongoDB');
+  }
+};
+
+export { connectDB, disconnectDB };
+
+// export default connectDB;
